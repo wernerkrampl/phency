4f7c8fa9743252484793657f8b70e8b2447e8973
krisbal97 <57330402+krisbal97@users.noreply.github.com>
2020-04-08 22:57:03 +0200

    Add files via upload

--- graphForGene.py
+++ graphForGene.py
@@ -1,6 +1,7 @@
 import networkx
 import obonet
-import matplotlib.pyplot as plt
+from networkx.drawing.nx_agraph import to_agraph
+import graphviz as gv
 
 #url = 'https://raw.githubusercontent.com/obophenotype/human-phenotype-ontology/master/hp.obo'
 #graph = obonet.read_obo(url)
@@ -25,56 +26,52 @@ class MiniGraph:
         if geneName == 'empty':
             self.geneName = ""
             return
-        else:  
+        else:
             self.geneName = geneName
             self.nodes = findGene(geneName)
             self.names = {term: id_to_name[term] for term in self.nodes}
             # in self.miniGraph is wanted subgraph
             self.miniGraph = graph.subgraph(self.nodes)
+            self.gvGraph = to_agraph(self.miniGraph)
+            self.gvGraph.node_attr['style']='filled'
             self.impactScore = {node: float(0.0)  for node in self.nodes}
             self.setLabels()
-            self.color = '#78b41f' 
+            self.gvGraph.node_attr['color']='#d0f0c0'
+            self.gvGraph.layout('dot')
 
     def setLabels(self):
         self.labels = {}
         for term, name in self.names.items():
             text = '['+ name +'; '+ str(self.impactScore[term]) +']'
-            self.labels[term] = text
+            self.gvGraph.get_node(term).attr['label'] = text
 
     #newScore is dictionary (node : score)
     def changeScore(self, newScore):
         self.impactScore = newScore
         self.setLabels()
 
-    def showGraph(self):
-        plt.subplot(122)
-        positions = networkx.spectral_layout(self.miniGraph)
-        #positions = networkx.planar_layout(self.miniGraph)
-        #trying different colors:  node_color=self.color
-        networkx.draw_networkx(self.miniGraph, pos=positions, node_color='#78b41f', labels=self.labels)
-        plt.show()
-
     def add(self, graph1, graph2):
+        #we use "copy" of graph1 as template for adding graph2 to it
         self.impactScore = graph1.impactScore.copy()
         graph2copy = graph2.impactScore.copy()
-        colorsToAdd = len(graph2copy)-1
-        self.color = ['#78b41f' for term in self.impactScore]
-        i = -1
-        for term in self.impactScore.items():
-            i+=1
+        colors={node: '#d0f0c0'  for node in graph1.nodes}
+        for term, score in self.impactScore.items():
             if term in graph2copy:
                 self.impactScore[term] += graph2copy[term]
                 graph2copy.pop(term)
-                self.colors[i] = '#b41f78'
-                colorsToAdd-=1
+                colors[term] = '#fceea7'
         for term, score in graph2copy.items():
             self.impactScore[term] = score
-        for c in range(colorsToAdd-1):
-            self.color.append('#1f78b4')
+            colors[term] = '#ffb6c1'
         self.nodes = self.impactScore.keys()
         self.names = {term: id_to_name[term] for term in self.nodes}
         self.miniGraph = graph.subgraph(self.nodes)
+        self.gvGraph = to_agraph(self.miniGraph)
         self.setLabels()
+        self.gvGraph.node_attr['style']='filled'
+        self.gvGraph.layout('dot')
+        for term in colors:
+            self.gvGraph.get_node(term).attr['color'] = colors[term]
 
 print("Enter genes:")
 genes = input().strip().split(" ")
@@ -83,8 +80,9 @@ if len(genes) == 1:
 else:
     m1 = MiniGraph(genes[0])
     m2 = MiniGraph(genes[1])
-    #m1.showGraph()
-    #m2.showGraph()
+    m1.gvGraph.draw('pokus1.png')
+    m2.gvGraph.draw('pokus2.png')
     m = MiniGraph()
     m.add(m1, m2)
-m.showGraph()
+m.gvGraph.draw('pokus1a2.png')
+
